0882e4c56adc9f03f623e1c2a3d81a5d97d436c7
marcogomes <marcogomesr@gmail.com>
2022-02-05 20:30:42 +0000

    first commit

---
+++ src/containers/TopSection/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from "styled-components"
+import tw from "twin.macro"
+
+const TopSectionContainer = styled.div`
+	${tw`
+		w-full
+		flex
+		h-screen
+		relative
+	`}
+`
+
+const TopSection = () => {
+  return <TopSectionContainer>
+	  
+  </TopSectionContainer>
+};
+
+export default TopSection;
